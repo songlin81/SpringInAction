773d07b64432b15860d2c141b2d2e4e0814ea337
Lin Song <song.lin@volvo.com>
2020-09-06 09:51:35 +0800

    MongoDB

---
+++ 106/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.mongodb.client.result.UpdateResult;
+import reactor.core.publisher.Mono;
+
+public interface UserService {
+}
---
+++ 106/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+}
