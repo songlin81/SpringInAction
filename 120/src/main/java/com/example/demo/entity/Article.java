e83e351f63597394e89d1c6ccadace4138311a6c
Lin Song <song.lin@volvo.com>
2020-09-09 17:13:43 +0800

    Mysql

---
+++ 120/src/main/java/com/example/demo/entity/Article.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+@Entity
+@Data
+public class Article extends BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    @NotEmpty(message = "Title can not be empty")
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+}
+
