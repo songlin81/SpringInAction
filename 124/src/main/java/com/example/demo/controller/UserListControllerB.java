d592ace1838079e670c402707789da626264bdff
Lin Song <song.lin@volvo.com>
2020-09-11 08:51:39 +0800

    ORM

---
+++ 124/src/main/java/com/example/demo/controller/UserListControllerB.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserListControllerB {
+    @Autowired
+    UserMapper userMapper;
+
+    @RequestMapping("/listall2")
+    public Page<User> getUserList(@RequestParam(value="pageNum",defaultValue="0")int pageNum, @RequestParam(value = "pageSize", defaultValue = "5") int pageSize)
+    {
+        PageHelper.startPage(pageNum, pageSize);
+        Page<User> userList= userMapper.getUserList();
+        return userList;
+    }
+}
