284edd7071445a3937b9bf9a7fe2745879e36f5a
Lin Song <song.lin@volvo.com>
2020-09-13 19:18:47 +0800

    JPA and Redis

---
+++ 134/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+	@Test
+	public void string() {
+		redisTemplate.opsForValue().set("num", 123);
+		redisTemplate.opsForValue().set("string", "some strings");
+		Object s = redisTemplate.opsForValue().get("num");
+		Object s2 = redisTemplate.opsForValue().get("string");
+		System.out.println(s);
+		System.out.println(s2);
+	}
+
+}
